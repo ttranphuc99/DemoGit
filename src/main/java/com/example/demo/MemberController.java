0058f11a6c8508ac2f985092780970a3f84bd3b7
Thien Phuc <ttran.phuc99@gmail.com>
2020-05-18 18:35:21 +0700

    [PhucTT] del member

--- src/main/java/com/example/demo/MemberController.java
+++ src/main/java/com/example/demo/MemberController.java
@@ -4,4 +4,8 @@ public class MemberController {
     public void create() {
         System.out.println("Create member");
     }
+
+    public void delete() {
+        System.out.println("Delete member");
+    }
 }
