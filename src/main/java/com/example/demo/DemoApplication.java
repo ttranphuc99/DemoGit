e7ab6950bf03b1e7d33e52100291aead1ed7f5ef
Thien Phuc <ttran.phuc99@gmail.com>
2020-05-19 18:31:41 +0700

    done function

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	/**
-	Run App
+	Run    App
 	 */
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
